cd4d9c1c869f53cc41e1bc1e6c3fe4b705892096
Guilherme Henrique S. Pereira <guilhermehsp26@gmail.com>
2024-05-13 15:47:52 -0300

    feat: add login functionality to the application

--- routes.js
+++ routes.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const routes = express.Router();
 const homeController = require('./src/controllers/homeController');
-const contatoController = require('./src/controllers/contatoController');
+const loginController = require('./src/controllers/loginController');
+const registerController = require('./src/controllers/registerController'); 
 
-routes.get('/',homeController.paginaInicial);
-routes.post('/', homeController.trataPost);
+routes.get('/',homeController.index);
 
-routes.get('/contato', contatoController.paginaInicial);
+// login routes
+routes.get('/login', loginController.index);//get login page
+routes.post('/login', loginController.login);//post login page
+
+// register routes
+routes.get('/register', registerController.index); //get register page
+routes.post('/register', registerController.register);//post register page
 
 module.exports = routes;
--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,3 +1,26 @@
+const Register = require('../models/RegisterModel');
+
 exports.index = (req, res) => { 
     res.render('login');
+}
+
+exports.login = async (req, res) => {
+    try {
+        const register = new Register(req.body);
+        await register.login();
+
+        if(register.errors.length > 0) {
+            req.flash('errors', register.errors);
+            req.session.save(() => res.redirect('/login'));
+            return;
+        }
+
+        req.flash('success', 'Seja bem-vindo(a). Você está logado(a).');
+        req.session.user = register.user;
+        req.session.save(() => res.redirect('/'));  
+
+    } catch (e) {
+        console.log(e);
+        return res.render('404');   
+    }
 }
