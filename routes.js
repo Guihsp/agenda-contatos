ad69edd54424931732467d27cf906a318ffa59dc
Guilherme Henrique S. Pereira <guilhermehsp26@gmail.com>
2024-05-14 14:41:57 -0300

    feat: Add contact page
    This commit adds a contact page to the application. The contactController.js file and the corresponding view files have been created. The contact page allows users to create or edit their contacts by filling out a form with fields for name, last name, email, and phone number. This feature enhances the functionality of the application by providing a way for users to manage their contacts.

--- routes.js
+++ routes.js
@@ -2,16 +2,21 @@ const express = require('express');
 const routes = express.Router();
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
-const registerController = require('./src/controllers/registerController'); 
+const registerController = require('./src/controllers/registerController');
+const contactController = require('./src/controllers/contactController'); 
 
 routes.get('/',homeController.index);
 
 // login routes
 routes.get('/login', loginController.index);//get login page
 routes.post('/login', loginController.login);//post login page
+routes.get('/logout', loginController.logout); //logout
 
 // register routes
 routes.get('/register', registerController.index); //get register page
 routes.post('/register', registerController.register);//post register page
+
+//contact routes
+routes.get('/contact', contactController.index);
 
 module.exports = routes;
