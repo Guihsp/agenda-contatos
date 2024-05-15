e88b32a0dcb175b60e1f5ad8764a10f4d32bb03b
Guilherme Henrique S. Pereira <guilhermehsp26@gmail.com>
2024-05-15 16:36:09 -0300

    refactor: Update contact routes and controller

--- routes.js
+++ routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const routes = express.Router();
-const homeController = require('./src/controllers/homeController');
-const loginController = require('./src/controllers/loginController');
-const registerController = require('./src/controllers/registerController');
-const contactController = require('./src/controllers/contactController'); 
+const homeController = require('./src/controllers/homeController'); // import home controller
+const loginController = require('./src/controllers/loginController'); // import login controller
+const registerController = require('./src/controllers/registerController'); // import register controller
+const contactController = require('./src/controllers/contactController'); // import contact controller
+const { loginRequired } = require('./src/middlewares/middleware'); // import middleware
 
-routes.get('/',homeController.index);
+// home routes
+routes.get('/',homeController.index); // get home page
 
 // login routes
 routes.get('/login', loginController.index);//get login page
@@ -17,6 +19,9 @@ routes.get('/register', registerController.index); //get register page
 routes.post('/register', registerController.register);//post register page
 
 //contact routes
-routes.get('/contact', contactController.index);
+routes.get('/contact', loginRequired, contactController.index); // get contact page
+routes.post('/contact/register', loginRequired, contactController.register); // post contact page
+routes.get('/contact/index/:id', loginRequired, contactController.editIndex); // get contact edit page
+routes.post('/contact/edit/:id', loginRequired, contactController.edit); // post contact edit page
 
 module.exports = routes;
